cd0310941aa7b334c29503af7744aaf78b27e45d
m-adams8 <adams.2666@osu.edu>
2025-04-14 08:59:55 -0400

    FIXED C4 views

--- Queries.sql
+++ Queries.sql
@@ -1,24 +1,6 @@
-SELECT A.F_Name, A.M_Name, A.L_Name
-FROM Author A
-JOIN Written_By WB ON A.Author_ID = WB.Author_ID
-JOIN Are_Purchased AP ON AP.ISBN = WB.ISBN
-JOIN Orders O ON O.Order_ID = AP.Order_ID
-JOIN Place PL ON PL.Order_ID = O.Order_ID
-JOIN Customer C ON C.Customer_ID = PL.Customer_ID
-WHERE C.Customer_ID IN (
-        SELECT C.Customer_ID
-        FROM Customer C
-        JOIN Place PL ON C.Customer_ID = PL.Customer_ID
-        JOIN Orders O ON O.Order_ID = PL.Order_ID
-        GROUP BY C.Customer_ID, C.F_Name, C.M_Name, C.L_Name, C.Email
-        HAVING SUM(O.Total) > (
-            SELECT AVG(Total)
-            FROM (
-                SELECT SUM(O.Total) AS Total
-                FROM Customer C
-                JOIN Place PL ON C.Customer_ID = PL.Customer_ID
-                JOIN Orders O ON O.Order_ID = PL.Order_ID
-                GROUP BY C.Customer_ID
-            ) CO
-        )
-);
+CREATE VIEW EmployeeOrderPerformance AS
+SELECT e.Employee_ID, e.F_Name, e.L_Name, COUNT(o.Order_ID) AS OrdersHandled, AVG(o.Total) AS AvgOrderValue, SUM(o.Total) AS TotalSales
+FROM Employee e JOIN Is_Fulfilled i ON e.Employee_ID = i.Employee_ID JOIN Orders o ON i.Order_ID = o.Order_ID
+GROUP BY e.Employee_ID, e.F_Name, e.L_Name;
+
+
